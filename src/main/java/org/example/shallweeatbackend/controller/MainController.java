ceb61e42d16c8099102207e088175de2c1dd1ff8
hyunji <phj9573@naver.com>
2024-06-26 15:14:31 +0900

    fixed untracked files

--- src/main/java/org/example/shallweeatbackend/controller/MainController.java
+++ src/main/java/org/example/shallweeatbackend/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-
     @GetMapping("/")
     public String home() {
         return "스위프 10팀";
