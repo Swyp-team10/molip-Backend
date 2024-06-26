6b29898ee08a0573885f0bb8c428ac5aff73286f
hyunji <phj9573@naver.com>
2024-06-26 15:09:01 +0900

    .gitignore 수정

---
+++ src/main/java/org/example/shallweeatbackend/controller/MainController.java
@@ -0,0 +1,13 @@
+package org.example.shallweeatbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "스위프 10팀";
+    }
+}
