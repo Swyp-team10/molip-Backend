d78e212d5c7d2a8c320b7ea82f29c23820a86475
hyunji <phj9573@naver.com>
2024-06-26 14:58:21 +0900

    initial commit

---
+++ src/main/java/org/example/shallweeatbackend/ShallweeatBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.shallweeatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShallweeatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShallweeatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/shallweeatbackend/ShallweeatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shallweeatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShallweeatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
