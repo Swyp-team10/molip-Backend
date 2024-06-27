b316f52cdaeb32a760335df00f1f5105fdc0a449
hyunji <phj9573@naver.com>
2024-06-27 14:11:49 +0900

    Test : 테스트 파일, 의존성 추가

--- src/test/java/org/example/shallweeatbackend/ShallweeatBackendApplicationTests.java
+++ src/test/java/org/example/shallweeatbackend/ShallweeatBackendApplicationTests.java
@@ -1,13 +1,25 @@
 package org.example.shallweeatbackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ShallweeatBackendApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+//	@Test
+//	void contextLoads() {
+//	}
 	@Test
-	void contextLoads() {
+	public void testDatabaseConnection() {
+		Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+		assertThat(result).isEqualTo(1);
 	}
 
 }
