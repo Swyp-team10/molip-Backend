93c742f2138752e4fa480122e1e2676bac525178
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2024-06-29 16:15:14 +0900

    Refactor : 엔드포인트를 /reissue에서 /api/v1/reissue-token으로 변경

--- src/main/java/org/example/shallweeatbackend/controller/ReissueTokenController.java
+++ src/main/java/org/example/shallweeatbackend/controller/ReissueTokenController.java
@@ -6,15 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.example.shallweeatbackend.service.ReissueTokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ReissueTokenController {
 
     private final ReissueTokenService reissueTokenService;
 
-    @PostMapping("/reissue")
+    @PostMapping("/reissue-token")
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
         return reissueTokenService.reissue(request, response);
     }
