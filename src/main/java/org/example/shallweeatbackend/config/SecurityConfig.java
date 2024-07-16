19cd254e8074e2ed1f0ae20e4369a62046e9c002
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2024-07-16 12:10:04 +0900

    Feature : https 연결 및 cors 설정

--- src/main/java/org/example/shallweeatbackend/config/SecurityConfig.java
+++ src/main/java/org/example/shallweeatbackend/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedOrigins(Collections.singletonList("https://molip-front.vercel.app"));
                         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
@@ -96,8 +96,8 @@ public class SecurityConfig {
                 }));
 
         // HTTP -> HTTPS로 리디렉션
-        /*http
-                .requiresChannel(channel -> channel.anyRequest().requiresSecure());*/
+        http
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure());
 
         return http.build();
     }
