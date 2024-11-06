1d5b3461a77995f413b82a8e6b4a6b0f6609985d
hyunji <phj9573@naver.com>
2024-11-06 15:12:13 +0900

    feat : 검색어 삭제 엔드포인트 수정(#74)

--- src/main/java/org/example/shallweeatbackend/controller/SearchController.java
+++ src/main/java/org/example/shallweeatbackend/controller/SearchController.java
@@ -8,6 +8,7 @@ import org.example.shallweeatbackend.service.SearchService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,8 @@ public class SearchController {
         return searchWordService.getSaveWordsList(principal.getProviderId());
     }
 
-    @DeleteMapping("/map/search")
-    public void deleteSearchWord(@AuthenticationPrincipal CustomOAuth2User principal, @RequestParam Long searchId){
+    @DeleteMapping("/map/search/{searchId}")
+    public void deleteSearchWord(@AuthenticationPrincipal CustomOAuth2User principal, @PathVariable Long searchId){
         if(principal == null)
             throw new IllegalStateException("사용자가 인증되지 않았습니다.");
 
