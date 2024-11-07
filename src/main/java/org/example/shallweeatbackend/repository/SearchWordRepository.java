aa12c3224ced95937f8e5b4bd9a0f910a9791a45
hyunji <phj9573@naver.com>
2024-11-08 03:19:37 +0900

    feat : 검색어 조회 반환 순서 변경, 검색어 삭제 시 응답 형식 변경

--- src/main/java/org/example/shallweeatbackend/repository/SearchWordRepository.java
+++ src/main/java/org/example/shallweeatbackend/repository/SearchWordRepository.java
@@ -9,4 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SearchWordRepository extends JpaRepository<SearchWord, Long> {
     List<SearchWord> findByUser(User user);
 
+    List<SearchWord> findByUserOrderByCreatedAtDesc(User user);
 }
